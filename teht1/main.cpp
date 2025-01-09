9bca305a18ed57d9f329697e3f66c4171fe15b5a
Are-Fin <aarreilmari.patrikainen@gmail.com>
2025-01-09 12:03:08 +0200

    teht1 valmis

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+int game(int);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int score = game(20);
+
+    cout << "Yrityksia " << score << endl;
+
     return 0;
+}
+
+int game(int maxnum){
+
+    srand(time(NULL));
+
+    if(maxnum < 2){
+        maxnum = 2;
+    }
+
+    int randArvo = rand() % maxnum + 1;
+    int arvaus = 0;
+    int score = 0;
+
+    cout << "Arvaa luku 1-" << maxnum << endl;
+
+    do {
+        string s = "null";
+        cin >> arvaus;
+        if (arvaus == randArvo){
+            s = "Arvasit oikein!";
+        } else if (arvaus < randArvo){
+            s = "Luku on suurempi";
+        } else if (arvaus > randArvo){
+            s = "Luku on pienempi";
+        }
+        cout << s << endl;
+        score++;
+
+    } while (arvaus != randArvo);
+
+    return score;
 }
